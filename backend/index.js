25c7f188fec588af247927bafe1779392a217937
ieeemace123-123 <ieeemace123@gmail.com>
2026-08-02 20:13:20 +0530

    Initial commit for Vercel deployment

---
+++ backend/index.js
@@ -0,0 +1,5 @@
+import app from "../src/server.js";
+
+// Vercel's Node.js runtime can invoke an Express app directly
+// as a request handler (it matches the (req, res) signature).
+export default app;
